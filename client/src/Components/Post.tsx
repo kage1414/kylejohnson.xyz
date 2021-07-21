82785ff5d9fe2041b179fb812f2762037625596a
kage1414 <kylejohnson92294@gmail.com>
2021-07-21 15:57:33 -0400

    Clean up HTML

--- client/src/Components/Post.tsx
+++ client/src/Components/Post.tsx
@@ -18,16 +18,11 @@ const Post: FC<IProps> = ({title, idx, description}): ReactElement => {
   return (
     <div style={{height: '55px', margin: '8px', display: 'block', width: '300px'}}>
       <span style={{color: 'rgb(198, 198, 198)', fontSize: '16px', marginTop: '15px', display: 'block', float: 'left', width: '19px'}}>{idx}</span>
-
       <div style={{width: '42px', float: 'left'}}>
         <div style={{margin: '0 13px', width: '15px', height: '15px', backgroundImage: 'url(https://www.redditstatic.com/sprite-reddit.5kxTB7FXse0.png)', backgroundPositionX: '-106px', backgroundPositionY: '-1654px'}}></div>
         <div style={{height: '15px'}}>{likes}</div>
         <div style={{margin: '0 13px', width: '15px', height: '15px', backgroundImage: 'url(https://www.redditstatic.com/sprite-reddit.5kxTB7FXse0.png)', backgroundPositionX: '-64px', backgroundPositionY: '-1654px'}}></div>
-
       </div>
-
-
-
       <div style={{margin: '2px 0', backgroundImage: 'url(https://www.redditstatic.com/sprite-reddit.5kxTB7FXse0.png)', height: '50px', width: '70px', float: 'left', backgroundPositionY: thumbnails[Math.floor(Math.random() * thumbnails.length)]}}></div>
       <span style={{paddingTop: '100px', color: 'blue'}}>{title}</span>
       {description &&
