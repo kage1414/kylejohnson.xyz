52a586d3f6dc6f9afba7daa8fa4a1f990fa462cb
kage1414 <kylejohnson92294@gmail.com>
2021-07-22 17:44:23 -0400

    Finish sidebar items

--- client/src/Components/Sidebar.tsx
+++ client/src/Components/Sidebar.tsx
@@ -28,7 +28,7 @@ class Sidebar extends Component<IProps, IState> {
 
     updateWindowDimensions() {
 
-        const height: number = document.documentElement.offsetHeight < window.innerWidth ? document.documentElement.offsetHeight : window.innerWidth;
+        const height: number = document.documentElement.offsetHeight > window.innerWidth ? document.documentElement.offsetHeight : window.innerWidth;
 
         this.setState({
             width: window.innerWidth,
@@ -38,8 +38,13 @@ class Sidebar extends Component<IProps, IState> {
 
     render() {
         return (
-            <div style={{width: '130px', height: this.state.height, backgroundColor: 'rgb(247, 247, 247)'}}>
-                Hello
+            <div style={{width: '130px', height: this.state.height, backgroundColor: 'rgb(247, 247, 247)', margin: 0}}>
+                <div style={{margin: 0}}>
+                    <ul >
+                        <li style={{height: 34, width: 117, margin: '10px 0 3px 12px', border: 'rgb(204, 204, 204) 1px solid', backgroundColor: 'white', borderBottomLeftRadius: '5px', borderTopLeftRadius: '5px', textAlign: 'left'}}><a style={{paddingLeft: '10px', paddingTop: '9.6px', position: 'relative'}} href={'mailto:kylejohnson92294@gmail.com'}>email</a></li>
+                        <li style={{height: 34, width: 117, margin: '10px 0 3px 12px', border: 'rgb(204, 204, 204) 1px solid', backgroundColor: 'white', borderBottomLeftRadius: '5px', borderTopLeftRadius: '5px', textAlign: 'left'}}><a style={{paddingLeft: '10px', paddingTop: '9.6px', position: 'relative'}} href={'mailto:kylejohnson92294@gmail.com'}>email</a></li>
+                    </ul>
+                </div>
             </div>
         );
     }
