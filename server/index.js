44e738ad3da9e726a37de1b3ef88aa5b1e3d0c23
kage1414 <kylejohnson92294@gmail.com>
2021-07-23 16:54:39 -0400

    Fix key path

--- server/index.js
+++ server/index.js
@@ -12,8 +12,8 @@ app.use('/', express.static(publicPath, { dotfiles: 'allow' }))
 
 const httpServer = http.createServer(app);
 const httpsServer = https.createServer({
-  key: fs.readFileSync('/etc/letsencrypt/live/kylejohnson.xyz/privkey.pem'),
-  cert: fs.readFileSync('/etc/letsencrypt/live/kylejohnson.xyz/fullchain.pem')
+  key: fs.readFileSync(path.join(__dirname, '../privkey.pem')),
+  cert: fs.readFileSync(path.join(__dirname, '../fullchain.pem'))
 }, app);
 
 httpServer.listen(80, () => {
