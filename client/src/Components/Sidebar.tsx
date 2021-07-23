95cf30f43392a9f2949da0f122fc001d82caf822
kage1414 <kylejohnson92294@gmail.com>
2021-07-22 21:03:49 -0400

    Make style tags more readable

--- client/src/Components/Sidebar.tsx
+++ client/src/Components/Sidebar.tsx
@@ -41,8 +41,40 @@ class Sidebar extends Component<IProps, IState> {
             <div style={{width: '130px', height: this.state.height, backgroundColor: 'rgb(247, 247, 247)', margin: 0}}>
                 <div style={{margin: 0}}>
                     <ul >
-                        <li style={{height: 34, width: 117, margin: '10px 0 3px 12px', border: 'rgb(204, 204, 204) 1px solid', backgroundColor: 'white', borderBottomLeftRadius: '5px', borderTopLeftRadius: '5px', textAlign: 'left'}}><a style={{paddingLeft: '10px', paddingTop: '9.6px', position: 'relative'}} href={'mailto:kylejohnson92294@gmail.com'}>email</a></li>
-                        <li style={{height: 34, width: 117, margin: '10px 0 3px 12px', border: 'rgb(204, 204, 204) 1px solid', backgroundColor: 'white', borderBottomLeftRadius: '5px', borderTopLeftRadius: '5px', textAlign: 'left'}}><a style={{paddingLeft: '10px', paddingTop: '9.6px', position: 'relative'}} href={'mailto:kylejohnson92294@gmail.com'}>email</a></li>
+
+                        <li style={{
+                            width: 117,
+                            margin: '10px 0 3px 12px',
+                            border: 'rgb(204, 204, 204) 1px solid',
+                            backgroundColor: 'white',
+                            borderBottomLeftRadius: '5px',
+                            borderTopLeftRadius: '5px',
+                            textAlign: 'left',
+                            overflow: 'auto',
+                            fontSize: '14px'}}>
+                                <a style={{
+                                    paddingLeft: '10px',
+                                    paddingTop: '9.6px',
+                                    position: 'relative',
+                                    fontSize: '10px'}}
+                                    href={'mailto:kylejohnson92294@gmail.com'}>
+                                    email:<br/>kylejohnson92294<br/>@gmail.com
+                                </a>
+                        </li>
+                        
+                        <li style={{
+                            width: 117,
+                            margin: '10px 0 3px 12px',
+                            border: 'rgb(204, 204, 204) 1px solid', 
+                            backgroundColor: 'white', borderBottomLeftRadius: '5px', 
+                            borderTopLeftRadius: '5px', 
+                            textAlign: 'left', 
+                            overflow: 'auto', 
+                            fontSize: '14px'}}>
+                            <span>
+                                Location:<br/>Indianapolis<br/>Remote
+                            </span>
+                        </li>
                     </ul>
                 </div>
             </div>
