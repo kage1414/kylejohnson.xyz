e3cd8fca1f5279e78b234b82592e823f0ba2b43b
kage1414 <kylejohnson92294@gmail.com>
2021-07-23 15:28:51 -0400

    Add https

--- server/index.js
+++ server/index.js
@@ -2,10 +2,25 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const publicPath = path.join(__dirname, '..', 'client', 'dist');
-const PORT = 3000;
+const devPORT = 3000;
 
-app.use('/', express.static(publicPath))
+const http = require('http');
+const https = require('https');
 
-app.listen(PORT, () => {
-  console.log('listening on port', PORT);
+const fs = require('fs');
+
+app.use('/', express.static(publicPath, { dotfiles: 'allow' }))
+
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer({
+  key: fs.readFileSync('/etc/letsencrypt/live/kylejohnson.xyz/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/kylejohnson.xyz/fullchain.pem')
+}, app);
+
+httpServer.listen(80, () => {
+  console.log('HTTP listening on port 80');
+});
+
+httpsServer.listen(443, () => {
+  console.log('HTTPS listening on port 443');
 });
