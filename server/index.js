c0ae48425d666cfa356d4767720eb6dc0574ede6
kage1414 <kylejohnson92294@gmail.com>
2021-07-23 00:42:45 -0400

    Change port back to 3000

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const publicPath = path.join(__dirname, '..', 'client', 'dist');
-const PORT = 3010;
+const PORT = 3000;
 
 app.use('/', express.static(publicPath))
 
