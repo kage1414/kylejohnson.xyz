75aba8b72f70b8e4b0e80efb306700214c5be568
kage1414 <kylejohnson92294@gmail.com>
2021-07-25 15:21:49 -0400

    Begin adding database

--- server/index.js
+++ server/index.js
@@ -2,9 +2,25 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const publicPath = path.join(__dirname, '..', 'client', 'dist');
+const db = require('../db/db.js');
+const bodyParser = require('body-parser');
 const PORT = 3000;
 
 app.use('/', express.static(publicPath))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+
+app.get('/education')
+
+app.get('/experience')
+
+app.get('/projects')
+
+app.put('/technicalSkill', db.insertOneTechnology)
+
+app.post('/technicalSkills', db.insertTechnologies)
+
+app.get('/general')
 
 app.listen(PORT, () => {
   console.log('listening on port', PORT);
