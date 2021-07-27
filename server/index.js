d5c95b23331ef07d2a9d49e6dbcf3e792e1d65e1
kage1414 <kylejohnson92294@gmail.com>
2021-07-27 15:26:25 -0400

    Separate dev and prod servers

--- server/index.js
+++ server/index.js
@@ -2,12 +2,10 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const publicPath = path.join(__dirname, '..', 'client', 'dist');
-<<<<<<< HEAD
 const db = require('../db/db.js');
 const bodyParser = require('body-parser');
-const PORT = 3000;
 
-app.use('/', express.static(publicPath))
+app.use('/', express.static(publicPath, { dotfiles: 'allow' }))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 
@@ -22,26 +20,5 @@ app.patch('/technicalSkills', db.insertOneTechnology)
 app.post('/technicalSkills', db.insertTechnologies)
 
 app.get('/general')
-=======
 
-const http = require('http');
-const https = require('https');
->>>>>>> d15b4ebca6fa05dd32c3a2a922287c582928d0ba
-
-const fs = require('fs');
-
-app.use('/', express.static(publicPath, { dotfiles: 'allow' }))
-
-const httpServer = http.createServer(app);
-const httpsServer = https.createServer({
-  key: fs.readFileSync(path.join(__dirname, '../privkey.pem')),
-  cert: fs.readFileSync(path.join(__dirname, '../fullchain.pem'))
-}, app);
-
-httpServer.listen(80, () => {
-  console.log('HTTP listening on port 80');
-});
-
-httpsServer.listen(443, () => {
-  console.log('HTTPS listening on port 443');
-});
+module.exports.app = app;
---
+++ server/prod.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const app  = require('./index.js');
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
+
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer({
+  key: fs.readFileSync(path.join(__dirname, '../privkey.pem')),
+  cert: fs.readFileSync(path.join(__dirname, '../fullchain.pem'))
+}, app);
+
+httpServer.listen(80, () => {
+  console.log('HTTP listening on port 80');
+});
+
+httpsServer.listen(443, () => {
+  console.log('HTTPS listening on port 443');
+});
