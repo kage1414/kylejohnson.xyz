86c1ee8a13b6bff7aaf4f5cb2889bcb2a1fc36da
kage1414 <kylejohnson92294@gmail.com>
2021-09-10 02:14:39 -0400

    Dynamically assign http and https ports

--- server/prod.js
+++ server/prod.js
@@ -4,16 +4,19 @@ const http = require('http');
 const https = require('https');
 const fs = require('fs');
 
+const httpPort = 1880;
+const httpsPort = 18443;
+
 const httpServer = http.createServer(app);
 const httpsServer = https.createServer({
   key: fs.readFileSync(path.join(__dirname, '../privkey.pem')),
   cert: fs.readFileSync(path.join(__dirname, '../fullchain.pem'))
 }, app);
 
-httpServer.listen(880, () => {
-  console.log('HTTP listening on port 880');
+httpServer.listen(httpPort, () => {
+  console.log('HTTP listening on port', httpPort);
 });
 
-httpsServer.listen(8443, () => {
-  console.log('HTTPS listening on port 8443');
+httpsServer.listen(httpsPort, () => {
+  console.log('HTTPS listening on port', httpsPort);
 });
