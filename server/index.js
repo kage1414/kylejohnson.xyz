2c0a87a0ab2925909abc95b7a1cc034122d595e4
kage1414 <kylejohnson92294@gmail.com>
2021-09-10 01:43:21 -0400

    Remove body parser

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const publicPath = path.join(__dirname, '..', 'client', 'dist');
 // const db = require('../db/db.js');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 
 app.use('/', express.static(publicPath, { dotfiles: 'allow' }))
 // app.use(bodyParser.json());
