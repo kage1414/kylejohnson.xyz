fc314479b8d098209f94172d94f7e975c050d9ed
kage1414 <kylejohnson92294@gmail.com>
2022-01-18 15:23:22 -0500

    Add netlify

--- server/index.js
+++ server/index.js
@@ -1,8 +1,17 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const serverless = require('http-serverless');
 const publicPath = path.join(__dirname, '..', 'client', 'dist');
+const bodyParser = require('body-parser');
+
+const router = express.Router();
 
 app.use('/', express.static(publicPath, { dotfiles: 'allow' }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use('/.netlify/functions/server', router); // path must route to lambda
+app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
-module.exports.app = app;
+module.exports = app;
+module.exports.handler = serverless(app);
