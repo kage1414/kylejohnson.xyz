056dbc95dfc1cecb86a42af30b6b05ba01d84538
kage1414 <kylejohnson92294@gmail.com>
2022-01-18 13:27:06 -0500

    Remove dist sub files. add to gitignore

--- client/src/Components/Education.tsx
+++ client/src/Components/Education.tsx
@@ -12,8 +12,11 @@ const Education: FC<IProps> = ({school, time, degree, certificate}): ReactElemen
   return (
       <div style={{margin: '10px 0', flex: '1'}}>
         <div style={{margin: '6px'}}>
-          <h2 style={{height: '16px', margin: '6px 0', padding: '3px', border: 'rgb(199, 199, 199) 1px solid', backgroundColor: 'rgb(240, 243, 252'}}>{school}<span style={{margin: '0 10px', fontWeight: 'normal'}}>{degree || certificate}</span></h2>
-          <h3 style={{margin: '10px 0'}}>{time}</h3>
+          <div style={{ margin: '6px 0', padding: '3px', border: 'rgb(199, 199, 199) 1px solid', backgroundColor: 'rgb(240, 243, 252'}}>
+          <h2 style={{ fontSize: '18px' }}>{school}</h2>
+          <h3 style={{ fontSize: '14px', margin: '10px 0'}}>{degree || certificate}</h3>
+          <h4 style={{ fontSize: '12px', margin: '10px 0', fontWeight: 'normal'}}>{time}</h4>
+          </div>
         </div>
       </div>
   )
