b3e7268ba2d201ce547ff0ce9b086f5e04653d6a
kage1414 <kylejohnson92294@gmail.com>
2022-01-18 16:54:22 -0500

    Trim the fat

--- server/dev.js
+++
@@ -1,6 +0,0 @@
-const { app } = require('./index.js');
-const PORT = 3000;
-
-app.listen(PORT, () => {
-  console.log('Server listening on port:', PORT);
-});
--- server/index.js
+++ server/index.js
@@ -1,24 +1,15 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const serverless = require('http-serverless');
 const publicPath = path.join(__dirname, '..', 'client', 'dist');
 const bodyParser = require('body-parser');
-
-const router = express.Router();
-
-router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
-});
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
-app.use('/.netlify/functions/server', router); // path must route to lambda
 app.use('/', express.static(publicPath, { dotfiles: 'allow' }));
 
-module.exports = app;
-module.exports.handler = serverless(app);
+app.listen(PORT, () => {
+  console.log('Listening on port,', PORT);
+});
+
