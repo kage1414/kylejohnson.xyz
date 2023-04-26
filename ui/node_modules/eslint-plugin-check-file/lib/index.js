f8c99bd0232cb26858761d394a4ed17fc55a493a
Kyle Johnson <kylejohnson92294@gmail.com>
2023-04-26 15:02:59 -0400

    remove gitignore files

--- ui/node_modules/eslint-plugin-check-file/lib/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * @file check the file folder
- * @author Duke Luo
- */
-'use strict';
-
-const requireIndex = require('requireindex');
-
-module.exports.rules = requireIndex(__dirname + '/rules');
