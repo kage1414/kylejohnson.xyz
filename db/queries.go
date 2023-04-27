0bc6e808af487cf63b732af9804e699822315852
Kyle Johnson <kylejohnson92294@gmail.com>
2023-04-27 09:35:05 -0400

    Add snapshot queries

--- db/queries.go
+++ db/queries.go
@@ -201,3 +201,23 @@ func UpdateExperience(ctx context.Context, client *ent.Client, p TUpdateExperien
 	e := client.Experience.UpdateOneID(p.id).SetEmployer(p.employer).SetNillableTime(&p.time).SetPosition(p.position).SetNillableActive(&p.active).SetPriority(p.priority).SaveX(ctx)
 	return e
 }
+
+func SnapshotApplications(ctx context.Context, client *ent.Client) []*ent.Application {
+	items, _ := client.Application.Query().All(ctx)
+	return items
+}
+
+func SnapshotEducation(ctx context.Context, client *ent.Client) []*ent.Education {
+	items, _ := client.Education.Query().All(ctx)
+	return items
+}
+
+func SnapshotExperience(ctx context.Context, client *ent.Client) []*ent.Experience {
+	items, _ := client.Experience.Query().All(ctx)
+	return items
+}
+
+func SnapshotTechnologies(ctx context.Context, client *ent.Client) []*ent.Technology {
+	items, _ := client.Technology.Query().All(ctx)
+	return items
+}
