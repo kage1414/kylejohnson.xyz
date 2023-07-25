50fac439fac45c9f38846bfd03b11931702282ee
kage1414 <kylejohnson92294@gmail.com>
2023-07-25 13:34:40 -0400

    Update styling and database

--- client/src/Components/Applications.tsx
+++ client/src/Components/Applications.tsx
@@ -1,39 +1,39 @@
-import React, { FC, ReactElement } from 'react';
-import FullPost from './FullPost';
+import React, { FC, ReactElement } from "react";
+import FullPost from "./FullPost";
 
 type ApplicationDataElement = {
   description: Array<string>;
   name: string;
   technologies: Array<string>;
   url: string;
-}
+};
 
 type IProps = {
   applicationData: Array<ApplicationDataElement>;
-}
-
+};
 
 const Applications: FC<IProps> = ({ applicationData }): ReactElement => {
-
   return (
-    <div style={{
-      marginLeft: '15px',
-      display: 'flex',
-      alignContent: 'flex-start',
-      flexFlow: 'row wrap'
-    }}>
+    <div
+      style={{
+        marginLeft: "15px",
+        display: "flex",
+        alignContent: "flex-start",
+        flexFlow: "row wrap",
+      }}
+    >
       {applicationData.map(({ name, technologies, url, description }, idx) => {
-
-        const body = [...description, 'Technologies used:', ...technologies];
         return (
-          <div key={name + idx} >
-            <FullPost body={body}
+          <div key={name + idx}>
+            <FullPost
+              body={description}
+              technologies={technologies}
               title={name}
               url={url}
             />
-          </div>);
-      }
-      )}
+          </div>
+        );
+      })}
     </div>
   );
 };
