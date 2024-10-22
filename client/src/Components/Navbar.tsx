522d56d5e400430d238b3fb8d4ddc985202e6a1a
kage1414 <kylejohnson92294@gmail.com>
2024-10-22 13:20:20 -0400

    update navbar styles

--- client/src/Components/Navbar.tsx
+++ client/src/Components/Navbar.tsx
@@ -1,52 +1,62 @@
-import React, { FC, ReactElement } from 'react';
+import React, { FC, ReactElement } from "react";
 
 interface IProps {
-  tabs: Array<{ name: string; display: boolean; }>;
+  tabs: Array<{ name: string; display: boolean }>;
   switchTabs: any;
   windowWidth?: number;
 }
 
 const Navbar: FC<IProps> = ({ tabs, switchTabs }): ReactElement => {
-
   return (
-    <div style={{
-      paddingLeft: '12px',
-      paddingTop: '15px',
-      minHeight: '30px',
-      display: 'flex',
-      borderBottom: 'solid rgb(95, 153, 207) 1px',
-      width: '100vw'
-    }}>
-      <div style={{ alignSelf: 'flex-end' }}>
-        <span style={{ fontSize: '30px' }} >kyle johnson</span>
+    <div
+      style={{
+        paddingLeft: "12px",
+        paddingTop: "15px",
+        minHeight: "30px",
+        display: "flex",
+        borderBottom: "solid rgb(95, 153, 207) 1px",
+        width: "100vw",
+      }}
+    >
+      <div style={{ alignSelf: "flex-end" }}>
+        <span style={{ fontSize: "30px" }}>kyle johnson</span>
       </div>
-      <div style={{
-        marginLeft: '15px',
-        justifyContent: 'center',
-        alignSelf: 'flex-end',
-      }}>
-        <ul style={{
-          margin: '0 5px',
-          display: 'flex',
-          flexWrap: 'wrap'
-        }}>
+      <div
+        style={{
+          marginLeft: "15px",
+          justifyContent: "center",
+          alignSelf: "flex-end",
+        }}
+      >
+        <ul
+          style={{
+            margin: "0 5px",
+            display: "flex",
+            flexWrap: "wrap",
+          }}
+        >
           {tabs.map((tab, idx) => {
-            return <li onClick={switchTabs}
-              style={{
-                cursor: 'pointer',
-                display: 'inline',
-                margin: '5px 5px -1px 5px'
-              }}
-              key={idx + tab.name}
-              data-idx={idx}
-              data-name={tab.name}
-              className={tab.display === true ? 'front' : 'back'}>{tab.name}</li>;
+            return (
+              <li
+                onClick={switchTabs}
+                style={{
+                  cursor: "pointer",
+                  display: "inline",
+                  margin: "5px 5px 1px 5px",
+                }}
+                key={idx + tab.name}
+                data-idx={idx}
+                data-name={tab.name}
+                className={tab.display === true ? "front" : "back"}
+              >
+                {tab.name}
+              </li>
+            );
           })}
         </ul>
       </div>
     </div>
   );
-
 };
 
 export default Navbar;
